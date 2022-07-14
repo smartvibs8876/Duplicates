5c4e74a2a158c5336919324a40b0572f55c686e8
smartvibs8876 <vibhavdhaimode@gmail.com>
2022-07-14 17:54:57 +0530

    Commented file reading

--- duplicates.py
+++ duplicates.py
@@ -1,25 +1,25 @@
 import os
 import sys
 
-#folderDict = dict()
-#
-#
-# try:
-#     for i in range( 1 , len(sys.argv) ):
-#         print(sys.argv[i])
-#         folderDict[ sys.argv[i] ] = os.listdir( sys.argv[i] )
-#
-# except Exception:
-#     print("Failed to load the files")
+folderDict = dict()
 
-def readFileContentsInADict(*files) -> dict:
-    fileDict = dict()
-    for file in files:
-        txtFile = open(file, "r")
-        fileContent = txtFile.read()
-        fileContentList = fileContent.split("\n")
-        fileDict[file] = fileContentList
-    return fileDict
+
+try:
+    for i in range( 1 , len(sys.argv) ):
+        print(sys.argv[i])
+        folderDict[ sys.argv[i] ] = os.listdir( sys.argv[i] )
+
+except Exception:
+    print("Failed to load the files")
+
+# def readFileContentsInADict(*files) -> dict:
+#     fileDict = dict()
+#     for file in files:
+#         txtFile = open(file, "r")
+#         fileContent = txtFile.read()
+#         fileContentList = fileContent.split("\n")
+#         fileDict[file] = fileContentList
+#     return fileDict
 
 def extractBasePackageName(packageName: str) -> str:
     tmp = packageName.split("-")
@@ -39,9 +39,9 @@ def extractBasePackageName(packageName: str) -> str:
     return extractedPackageName
 
 
-fileDict = readFileContentsInADict(r"C:\Users\AVVVER744\DocumentsVibs\OpenCETraining\Helper\linux-64.txt",r"C:\Users\AVVVER744\DocumentsVibs\OpenCETraining\Helper\noarch.txt")
-for key in fileDict.keys():
-    packages = fileDict[key]
+#fileDict = readFileContentsInADict(r"C:\Users\AVVVER744\DocumentsVibs\OpenCETraining\Helper\linux-64.txt",r"C:\Users\AVVVER744\DocumentsVibs\OpenCETraining\Helper\noarch.txt")
+for key in folderDict.keys():
+    packages = folderDict[key]
     packagesConsidered = list()
     for i in range(0,len(packages)):
         if( (packages[i] in packagesConsidered) or ( ".conda" not in packages[i] )):
