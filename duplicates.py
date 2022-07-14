a192707ae3a819af2f06416e546f7a9b93140de5
smartvibs8876 <vibhavdhaimode@gmail.com>
2022-07-14 11:18:43 +0530

    Uncommented

--- duplicates.py
+++ duplicates.py
@@ -12,25 +12,25 @@ for i in range( 1 , len(sys.argv) ):
 
 
 print(folderDict)
-# for key in folderDict.keys():
-#     packages = folderDict[key]
-#     packagesConsidered = list()
-#     for i in range(0,len(packages)):
-#         if(packages[i] in packagesConsidered):
-#             continue
-#         try:
-#             packageName = packages[i].split("-")[0]
-#         except Exception:
-#             print("Error in extracting package name")
-#             break
-#         duplicates = False
-#         duplicateList = list()
-#         duplicateList.append(packages[i])
-#         packagesConsidered.append(packages[i])
-#         for j in range(i+1,len(packages)):
-#             if(packageName in packages[j]):
-#                 duplicates = True
-#                 duplicateList.append(packages[j])
-#                 packagesConsidered.append(packages[i])
-#         if(duplicates == True):
-#             print(duplicateList)
+for key in folderDict.keys():
+    packages = folderDict[key]
+    packagesConsidered = list()
+    for i in range(0,len(packages)):
+        if(packages[i] in packagesConsidered):
+            continue
+        try:
+            packageName = packages[i].split("-")[0]
+        except Exception:
+            print("Error in extracting package name")
+            break
+        duplicates = False
+        duplicateList = list()
+        duplicateList.append(packages[i])
+        packagesConsidered.append(packages[i])
+        for j in range(i+1,len(packages)):
+            if(packageName in packages[j]):
+                duplicates = True
+                duplicateList.append(packages[j])
+                packagesConsidered.append(packages[i])
+        if(duplicates == True):
+            print(duplicateList)
